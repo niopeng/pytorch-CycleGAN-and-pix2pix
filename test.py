3ab0abf3cf45bdeb3499dec1278c54c38ac2647b
nio <niopeng@hotmail.com>
2020-09-27 14:37:50 -0700

    test script

--- test.py
+++ test.py
@@ -59,6 +59,9 @@ if __name__ == '__main__':
     for i, data in enumerate(dataset):
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
+        print(data['A'].shape)
+        print(a)
+        # for i in range()
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
